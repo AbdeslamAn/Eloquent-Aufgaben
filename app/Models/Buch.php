8c5dd8b7ebc5d3325803dc10458b30a8bb562657
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-25 09:08:38 +0000

    *belongsTo()* Definition der Beziehung im Buch-Modell (viel-zu-eins) / Definition of the Relationship in the Book Model (Many-to-one)

--- app/Models/Buch.php
+++ app/Models/Buch.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Buch extends Model
 {
-    //
+    use HasFactory;
+
+    public function autor()
+    {
+        return $this->belongsTo(Autor::class);
+    }
+
 }
