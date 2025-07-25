f0cf41123ef6dce67c9a5a953bb081b2e2191c0b
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-25 09:31:59 +0000

    Fakedaten erstellen / Create feke data

--- database/factories/BuchFactory.php
+++ database/factories/BuchFactory.php
@@ -17,7 +17,13 @@ class BuchFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(3),
+            'beschreibung' => fake()->paragraph,
+            'created_at' => fake()->dateTimeBetween('-2 years'),
+            'updated_at' => function (array $attributes)
+            {
+                return fake()->dateTimeBetween($attributes['created_at']);
+            }
         ];
     }
 }
