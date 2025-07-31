0ae2700d1c7ddd552b970862c1e24ebaedd49c47
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-07-31 20:51:18 +0000

    Schreibe einen Local Scope scopeAmBestenAutor im Modell Autor, der die Autoren nach der Anzahl ihrer Bücher absteigend sortiert. / Add scopeAmBestenAutor

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,6 +14,12 @@ class Autor extends Model
     public function buch()
     {
         return $this->hasMany(Buch::class);
-        
+
+    }
+
+    public function scopeAmBestenAutor(Builder $query): Builder
+    {
+        return $query->withCount('buch')// zählt die Bücher pro Autor
+                    ->orderBy('buch_count', 'desc');// sortiert nach der Anzahl absteigend
     }
 }
